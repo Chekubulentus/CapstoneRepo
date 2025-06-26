41e632fcb61e9db8dc326458bba242c4955ead3a
Paappaatt <barrios.edelson.quiling@gmail.com>
2025-06-26 18:31:26 +0800

    Adding Database
    Adding Racquel Database Context

---
+++ PharmacyBackend/PharmacyBackend/PharmacyBackend/DataContext/PharmacyContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PharmacyBackend.DataContext
+{
+    public class PharmacyContext : DbContext
+    {
+        public PharmacyContext(DbContextOptions<PharmacyContext> options) : base(options) { }
+    }
+}
--- PharmacyBackend/PharmacyBackend/PharmacyBackend/Program.cs
+++ PharmacyBackend/PharmacyBackend/PharmacyBackend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PharmacyBackend.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PharmacyContext>(x => x.UseNpgsql(
+    builder.Configuration.GetConnectionString("RacquelDB")));
 
 var app = builder.Build();
 
